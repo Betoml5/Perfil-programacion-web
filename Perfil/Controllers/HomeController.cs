e83d6494f11b16c3f58e473fceef7853b1a0df4f
Betoml5 <betoml5@hotmail.com>
2023-09-26 20:24:22 -0600

    Agregar archivos de proyecto.

---
+++ Perfil/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Perfil.Models.ViewModels;
+
+namespace Perfil.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            IndexViewModel vm = new()
+            {
+                Id = 1,
+                Name = "Alberto",
+                LastName = "Martinez",
+                ControlNumber = "221GC057",
+                Phone = "8611262242",
+                Career = "Ing. Sistemas Computacionales"
+            };
+
+
+
+            return View(vm);
+        }
+    }
+}
---
+++ Perfil/Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Perfil.Models.ViewModels
+{
+    public class IndexViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string ControlNumber { get; set; }
+        public string Career { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
